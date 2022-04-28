28235ecd680e3044aeb8e9c89bf6e3702ded2fa6
Nathaniel Krok <hammerman1010@gmail.com>
2022-04-28 17:47:55 -0500

    Finish DrawerLayout

--- app/src/main/java/com/example/scheduleu/MainActivity.java
+++ app/src/main/java/com/example/scheduleu/MainActivity.java
@@ -1,14 +1,89 @@
 package com.example.scheduleu;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    private ArrayAdapter<String> navAdapter;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        drawerList = findViewById(R.id.navList);
+
+        navAdapter = new ArrayAdapter<>(this, R.layout.nav_drawer_item, new ArrayList<String>());
+        drawerList.setAdapter(navAdapter);
+        drawerList.setOnItemClickListener((parent, view, position, id) -> selectDrawerItem(position));
+
+        drawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawerLayout,         /* DrawerLayout object */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        );
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+
+        navAdapter.add("Events");
+        navAdapter.add("Appointments");
+        navAdapter.add("Public Safety");
+        navAdapter.notifyDataSetChanged();
+    }
+
+    private void selectDrawerItem(int position) {
+        // change activity
+        Log.d(TAG, "selectDrawerItem: Selected drawer item " + position);
+
+        drawerLayout.closeDrawer(drawerList);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Check drawer first!
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            Log.d(TAG, "onOptionsItemSelected: mDrawerToggle " + item);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 }
